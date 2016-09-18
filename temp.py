2da7d8958af19feaa1776802733555d1154ed601
jigga301 <429527975@qq.com>
2016-09-18 23:25:43 +0800

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("#       #       ######      $               $")
+print(" #     #           #         $      $      $")
+print("   #  #            #          $    $ $    $" )
+print("    #              #           $   $ $   $")
+print("    #           #  #            $ $   $ $")
+print("    #            ##              $     $  ")
